473c2547e6d3001a76746ca632ae064a96b32844
rajpandey010 <rajpandey0925@gmail.com>
2025-02-17 10:27:37 +0530

    updated code

--- Consumer_Project/src/main/java/com/consumer/Consumer_Project/ConsumerProjectApplication.java
+++ Consumer_Project/src/main/java/com/consumer/Consumer_Project/ConsumerProjectApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ConsumerProjectApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumerProjectApplication.class, args);
 	}
-
 }
--- Consumer_Project/src/main/java/com/consumer/Consumer_Project/service/KafkaMessageConsumerService.java
+++ Consumer_Project/src/main/java/com/consumer/Consumer_Project/service/KafkaMessageConsumerService.java
@@ -5,47 +5,35 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.util.Date;
+
+
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 @Service
 public class KafkaMessageConsumerService {
 
 
-//    Logger LOG = LoggerFactory.getLogger(KafkaMessageConsumerService.class);
     @Autowired
     MessageRepository messageRepository;
-//    Date date = new Date();
-//    int intial =  date.getSeconds();
 
 //    String dummyApiUrl = "https://nx51np36-2707.inc1.devtunnels.ms/secured/apigateway/bulkapi";
-//    String dummyApiUrl = "http://localhost:2707/secured/apigateway/bulkapi";
-      String dummyApiUrl = "http://node-app:2707/secured/apigateway/bulkapi";
-      HttpClient client = HttpClient.newHttpClient();
+    String dummyApiUrl = "http://localhost:2707/secured/apigateway/bulkapi";
+//      String dummyApiUrl = "http://node-app:2707/secured/apigateway/bulkapi";
+//      HttpClient client = HttpClient.newHttpClient();
+      RestTemplate apiRestTemplate = new RestTemplate();
+
+
+
     public final ExecutorService executorService = Executors.newFixedThreadPool(10);
 
-    Date date = new Date();
-    int initial =  date.getSeconds();
 
     @KafkaListener(topics = "first", containerFactory = "kafkaListenerContainerFactory")
     public void consumeMessage(List<String> kafkaMessage) {
-            ThreadGroup threadGroup = Thread.currentThread().getThreadGroup();
-            int threadCount = threadGroup.activeCount();
-            System.out.println("The number of threads are => "+threadCount);
-            System.out.println(Thread.currentThread());
             kafkaMessage.forEach(kafkaMessageJson -> {
                 executorService.submit(() ->{
                     try {
-
                         // Send message to external API and get response
-                        RestTemplate apiRestTemplate = new RestTemplate();
                         String response = apiRestTemplate.postForObject(dummyApiUrl, "", String.class);
 
 //                        HttpRequest request = HttpRequest.newBuilder()
@@ -62,3 +50,26 @@ public class KafkaMessageConsumerService {
             });
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//            ThreadGroup threadGroup = Thread.currentThread().getThreadGroup();
+//            int threadCount = threadGroup.activeCount();
+//            System.out.println("The number of threads are => "+threadCount);
